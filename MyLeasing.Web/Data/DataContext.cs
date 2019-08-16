7358dd3e28345bdcd40b9f597372e538bcb6cac5
Luis Eduardo Gonzàelez  Vargas <luedgova1967@gmail.com>
2019-08-16 07:56:28 -0500

    Initial Db, Class Owner and ConnectionString

---
+++ MyLeasing.Web/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿namespace MyLeasing.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MyLeasing.Web.Data.Entities;
+
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Owner> Owners { get; set; }
+    }
+}
